1ae0a1ff86af72461405ebb6c73e49724d0f8a60
leeneil <lishantreasure@gmail.com>
2020-03-12 14:20:40 -0700

    add new args options

--- train_lstm.py
+++ train_lstm.py
@@ -21,6 +21,7 @@ class LSTMDataset(Dataset):
         self.n_dict = n_dict
         self.cuda = cuda
         self.seqs = np.random.randint(0, 19, (n, self.max_seq_size))
+        self.rand_idxs = [np.random.permutation(self.max_seq_size) for i in range(n)]
 
     def onehot(self, x):
         z = torch.zeros(len(x), self.n_dict)
@@ -34,15 +35,15 @@ class LSTMDataset(Dataset):
     def __getitem__(self, idx):
         seq = self.seqs[idx, :]
         x = self.onehot(seq)
-        rand_idx = np.random.permutation(len(seq))
+        rand_idx = self.rand_idxs[idx]
         y = torch.from_numpy(np.argsort(np.arange(len(seq))[rand_idx]))
         A = (np.repeat(rand_idx.reshape(-1, 1), rand_idx.shape[0], 1) - np.repeat(rand_idx.reshape(1, -1), rand_idx.shape[0], 0))
         A = 1.0 * (abs(A) == 1)
-        B = 1*np.random.rand(rand_idx.shape[0], self.n_features)
+        B = 0.01*np.random.rand(rand_idx.shape[0], self.n_features)
         for i in range(B.shape[0]):
             j = seq[rand_idx[i]]
             B[i,j] = B[i,j] + 1
-        B = B / np.sum(B, axis=1, keepdims=True)
+        #B = B / np.sum(B, axis=1, keepdims=True)
         C = torch.cat((torch.from_numpy(A), torch.from_numpy(B)), dim=1).float()
         if self.cuda:
             C = C.cuda()
@@ -62,11 +63,11 @@ class LSTMSeq(nn.Module):
         self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
 
     def forward(self, sequence, features):
-        fc_out = self.fc(features.view(-1))
+        fc_out = torch.sigmoid(self.fc(features.view(-1)))
         h0 = fc_out[0:self.hidden_dim]
         c0 = fc_out[self.hidden_dim:]
         lstm_out, _ = self.lstm(sequence.view(-1, 1, self.embedding_dim), (h0.view(1,1,-1), c0.view(1,1,-1)))
-        tag_out = self.hidden2tag(lstm_out.view(-1, self.hidden_dim))
+        tag_out = self.hidden2tag(torch.sigmoid(lstm_out.view(-1, self.hidden_dim)))
         return tag_out
 
     
@@ -102,7 +103,9 @@ def val(model, dataset):
 def parse_args():
     p = argparse.ArgumentParser(description=__doc__)
     p.add_argument("--n_train", "-n", type=int, default=10, help="Number of training samples")
+    p.add_argument("--n_val", type=int, default=10, help="Number of val samples")
     p.add_argument("--n_epoch", "-e", type=int, default=100, help="Number of training epoch")
+    p.add_argument("--n_seq", "-l", type=int, default=128, help="Max sequence length")
     p.add_argument("--lr", type=float, default=0.1, help="Learning rate")
     p.add_argument("--gpu", "-g", action="store_true", help="Use GPU")
     return p.parse_args()
@@ -110,13 +113,15 @@ def parse_args():
     
 def main():
     args = parse_args()
-    dataset = LSTMDataset(args.n_train, 128, cuda=args.gpu)
-    model = LSTMSeq(148*128, 20, 1024, 128)
+    l = args.n_seq
+    train_dataset = LSTMDataset(args.n_train, l, cuda=args.gpu)
+    model = LSTMSeq((l+20)*l, 20, 1024, l)
     if args.gpu:
         model = model.cuda()
-    model = train(model, dataset, n_epoch=args.n_epoch, lr=args.lr)
-    val(model, dataset)
-
+    model = train(model, train_dataset, n_epoch=args.n_epoch, lr=args.lr)
+    val(model, train_dataset)
+    val_dataset = LSTMDataset(args.n_val, l, cuda=args.gpu)
+    val(model, val_dataset)
 
 if __name__ == "__main__":
     main()
